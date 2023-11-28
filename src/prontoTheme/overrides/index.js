f1f6453327f8d07383290545300f6b8eec34174d
AnushkaWijegoonawardana97 <anushkaduwolka123@gmail.com>
2023-11-28 21:04:25 +0530

    adding comments and documentations

--- src/prontoTheme/overrides/index.js
+++ src/prontoTheme/overrides/index.js
@@ -1,7 +1,6 @@
 import Backdrop from "./Backdrop";
 import Button from "./Button";
 import Card from "./Card";
-import Paper from "./Paper";
 import Typography from "./Typography";
 
 // Function to combine and return overrides for multiple components
@@ -9,7 +8,6 @@ export default function ComponentsOverrides(theme) {
   // Combining overrides for different components
   return Object.assign(
     Card(theme), // Overrides for MuiCard component
-    Paper(theme), // Overrides for MuiPaper component
     Button(theme), // Overrides for MuiButton component
     Backdrop(theme), // Overrides for MuiBackdrop component
     Typography(theme) // Overrides for MuiTypography component
